65f5fcd9fd6823d4063dfb052a4def12a3e89f22
Taisir <taisirisawesome@gmail.com>
2023-05-06 13:39:38 -0400

    Implemented SearchBar

--- my-app/src/components/About /About.js
+++ my-app/src/components/About /About.js
@@ -1,6 +1,7 @@
+import "./About.css";
 function About() {
   return (
-    <div>
+    <div class="about-page">
       <h1>About Page</h1>
       <p>This is the about page</p>
     </div>
--- my-app/src/components/SearchBar/SearchBar.js
+++ my-app/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./SearchBar.css";
 
 function SearchBar(props) {
   return (
-    <div>
-      <input type="text" placeholder="Search..." onChange={props.onChange} />
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={props.onChange}
+        className="search-input"
+      />
+      <button className="search-button" onClick={props.onSearch}>
+        Search
+      </button>
     </div>
   );
 }
