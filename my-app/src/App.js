5719976a665e18e7d30a81b99d258c5fd89194d7
ogtc07 <owengtchung@outlook.com>
2023-05-06 18:46:35 -0400

    ProductCard change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import About from "./components/About/About";
 import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
     <div>
       <Header />
+      <About />
+
+      <SearchBar />
     </div>
   );
 }
