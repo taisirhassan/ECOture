bcad4eabc5ad86afccde40747400e2a1dc661de6
Joy <joyz030707@gmail.com>
2023-05-07 04:07:02 -0400

    search bar

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Landing from "./pages/Landing";
+import SearchBar from "./components/SearchBar/SearchBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/" element={<SearchBar />} />
       </Routes>
     </Router>
   );
--- my-app/client/src/components/SearchBar/SearchBar.js
+++ my-app/client/src/components/SearchBar/SearchBar.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+import { scrapeName, scrapeImage, scrapeMaterials} from "./scraper.js";
 import "./SearchBar.css";
+import ProductCard from "../ProductCard/ProductCard.js";
 
-function SearchBar(props) {
+const SearchBar = () => {
+  const [itemUrl, setItemUrl] = useState('');
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [materials, setMaterials] = useState({});
+
+  //const [search, setSearch] = useState(false);
+
+  const onChange = event => {
+    setItemUrl(event.target.value);
+  }
+
+  const onSearch = async () => {
+    console.log(itemUrl)
+    let name = await scrapeName(itemUrl);
+    setName(name);
+    /*let image = await scrapeImage(itemUrl);
+    setImage(image);
+    let materials = await scrapeMaterials(itemUrl);
+    setMaterials(materials);*/
+  }
+  
   return (
     <div className="search-container">
       <input
         type="text"
         placeholder="Search..."
-        onChange={props.onChange}
+        onChange={onChange}
         className="search-input"
       />
-      <button className="search-button" onClick={props.onSearch}>
+      <button className="search-button" onClick={onSearch}>
         Search
       </button>
+      <ProductCard
+        name={name}
+        image={image}
+        materials={materials}
+      ></ProductCard>
     </div>
   );
 }
