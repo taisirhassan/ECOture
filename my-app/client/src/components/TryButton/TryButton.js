6701c82ae5ba8725c457593255b130ce9c42e43f
ogtc07 <owengtchung@outlook.com>
2023-05-07 03:37:08 -0400

    Merge branch 'main' of https://github.com/taisirhassan/MetHacks-2023-

---
+++ my-app/client/src/components/TryButton/TryButton.js
@@ -0,0 +1,12 @@
+import "./TryButton.css";
+function TryButton() {
+  return (
+    <div className = "button-container"> 
+    <button className ="Button" >
+        Give it a try
+    </button>
+    </div>
+  );
+}
+
+export default TryButton;
